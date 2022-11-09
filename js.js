4f638763975389cdd305bafb1035fdf28a0cfce0
vangtt <giangthanhvan12@gmail.com>
2022-11-09 21:10:19 +0700

    BTVN2-js

---
+++ js.js
@@ -0,0 +1,4 @@
+let inputWidth=parseInt(prompt("Input the rectagle's width!"));
+let inputHeight=parseInt(prompt("Input the rectagle's height!"));
+let area = inputWidth*inputHeight;
+document.write("Rectagle's area is " + area);
